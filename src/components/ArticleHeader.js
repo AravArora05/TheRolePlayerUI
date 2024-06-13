714a0c89dd334860da6125fb15d3137056e18a5a
AravArora05 <aravarora05@gmail.com>
2024-06-13 15:46:04 -0400

    Neccesary Files Deleted

--- src/components/ArticleHeader.js
+++ src/components/ArticleHeader.js
@@ -11,7 +11,7 @@ const ArticleHeader = ({article}) => {
             <h2 className="article-header-title">{article.title}?</h2>
             <p className="article-header-author">By {article.authorName}</p>
             <p className="article-header-date">{article.date}</p>
-            <img className="article-image-url" src="https://wehco.media.clients.ellingtoncms.com/imports/adg/photos/207239636_207239582-0f78c263fbbc4e549a4da7ce818916e4_t800.jpg?90232451fbcadccc64a17de7521d859a8f88077d" alt="Jalen Brunson" />
+            <img className="article-image-url" src={article.imageSrc} alt={article.alt}/>
             <figcaption className="article-image-caption">
                 {article.caption.figcaption}
                 <span className="article-image-creator-credits">{`Creator: ${article.caption.creator} | Credit: ${article.caption.credit}`}</span>
